d391ca3c9d06fccfa91ada89b08e57ea6c30f6e7
SofianSaleh <sofiane.saleh11@gmail.com>
2024-01-06 11:18:04 +0200

    [Feat]: Added the Item Group Fast Select

---
+++ posawesome/public/js/posapp/components/pos/ItemGroupMultiSelect.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <v-btn
+      v-for="groupName in itemGroups"
+      :key="groupName"
+      medium
+      color="primary"
+      ref="gBtnRef"
+      style="padding: 4px"
+      class="ms-2 mb-2"
+      @click="triggerSelect($event, groupName)"
+    >
+      {{ groupName }}
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ItemGroupMultiSelect",
+  props: ["itemGroups", "label"],
+  methods: {
+    triggerSelect(event, groupName) {
+      console.log(event, groupName);
+      console.log(this.$refs);
+      this.$emit("click", event, groupName);
+    },
+  },
+};
+</script>
