8a37d0da229cafac637a88abd8da4b4cdb804fe1
omarjaber <omar.ja93@gmail.com>
2024-11-10 15:09:26 +0200

    auto generate card number

--- posawesome/posawesome/doctype/related_customer/related_customer.py
+++ posawesome/posawesome/doctype/related_customer/related_customer.py
@@ -5,6 +5,8 @@ import frappe
 from frappe import _
 from frappe.model.document import Document
 from frappe.utils import cint, cstr, flt, nowdate, comma_and, date_diff, getdate, time_diff, time_diff_in_hours
+import random
+
 
 class RelatedCustomer(Document):
     def validate(self):
@@ -16,3 +18,17 @@ class RelatedCustomer(Document):
             frappe.msgprint(_("The card has expired. Please use a valid card."))
         else:
             self.enabled=1
+
+    def before_save(self):
+        if not self.card_number:
+            while True:
+                # Generate random 10-digit number with prefix "PO"
+                random_number = f"PO{random.randint(1000000000, 9999999999)}"
+                
+                # Check if this card_number already exists
+                existing = frappe.db.exists("Related Customer", {"card_number": random_number})
+                
+                # If not exists, assign it to card_number and break the loop
+                if not existing:
+                    self.card_number = random_number
+                    break
