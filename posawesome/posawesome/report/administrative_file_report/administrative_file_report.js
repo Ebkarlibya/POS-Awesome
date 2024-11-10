5d76535cfee3208d2da028d10c89df7f39b343d6
omarjaber <omar.ja93@gmail.com>
2024-11-10 14:59:59 +0200

    edits

---
+++ posawesome/posawesome/report/administrative_file_report/administrative_file_report.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2024, Youssef Restom and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Administrative File Report"] = {
+	"filters": [
+
+	]
+};
