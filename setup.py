7b205b558c67ed9378a20dfc26c48fe3d1339e3b
Sofian Saleh <56444632+SofianSaleh@users.noreply.github.com>
2026-03-17 11:08:32 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
+import re
 
 with open("requirements.txt") as f:
     install_requires = f.read().strip().split("\n")
 
 # get version from __version__ variable in posawesome/__init__.py
-from posawesome import __version__ as version
+# from posawesome import __version__ as version
+with open("posawesome/__init__.py", "r", encoding="utf-8") as f:
+    version = re.search(
+        r'__version__\s*=\s*[\'"]([^\'"]+)[\'"]',
+        f.read()
+    ).group(1)
 
 setup(
     name="posawesome",
