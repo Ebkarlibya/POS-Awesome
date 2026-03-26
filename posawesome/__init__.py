d8c6fbecef7a05c62c990dc4a16bb2a5e51cc7ab
Sofian Saleh <56444632+SofianSaleh@users.noreply.github.com>
2026-03-26 09:34:42 +0100

    Comment out unused imports and console function
    Comment out future imports and console function.

--- posawesome/__init__.py
+++ posawesome/__init__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-import frappe
+# from __future__ import unicode_literals
+# import frappe
 
 
 __version__ = "4.6.4"
 
 
-def console(*data):
-    frappe.publish_realtime("toconsole", data, user=frappe.session.user)
+# def console(*data):
+#     frappe.publish_realtime("toconsole", data, user=frappe.session.user)
